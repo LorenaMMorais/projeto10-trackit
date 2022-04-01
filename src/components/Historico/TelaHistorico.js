47ca3f480e49fb0f8adfac624c0a39e91d1ff640
Lorena Morais <lorenamorais@alunos.utfpr.edu.br>
2022-04-01 20:46:47 -0300

    Add layou TelaHistorico

--- src/components/Historico/TelaHistorico.js
+++ src/components/Historico/TelaHistorico.js
@@ -1,7 +1,19 @@
+import { Corpo } from '../../assets/css/estilos';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import { ConteudoHistorico, Progresso, Titulo, TituloHistorico } from './estilo';
+
 export default function TelaHistorico(){
     return(
-        <>
-        
-        </>
+        <Corpo>
+            <Header/>
+            <ConteudoHistorico>
+                <TituloHistorico>
+                    <Titulo>Histórico</Titulo>
+                    <Progresso>Em breve você poderá ver o histórico dos seus hábitos aqui!</Progresso>
+                </TituloHistorico>
+            </ConteudoHistorico>
+            <Footer/>
+        </Corpo>
     );
 }
---
+++ src/components/Historico/estilo.jsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const ConteudoHistorico = styled.div`
+    width: 91%;
+    padding: 0 17px 430px 20px; 
+    gap: 10px;
+
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    flex-direction: column;
+
+    background: #F2F2F2;
+`
+export const TituloHistorico = styled.div`
+    margin-top: 70px;
+    box-sizing: border-box;
+`
+export const Titulo = styled.h1`
+    font-family: 'Lexend Deca';
+    font-weight: 400;
+    font-size: 22.976px;
+
+    color: #126BA5;
+`
+export const Progresso = styled.p`
+    font-family: 'Lexend Deca';
+    font-weight: 400;
+    font-size: 17.976px;
+
+    color: #BABABA;
+`
